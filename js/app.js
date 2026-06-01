89b096488b887e8a540bf781d4d33c3ffffb7e97
Rafakkj0 <134242702+Rafakkj0@users.noreply.github.com>
2026-06-01 19:47:59 -0300

    teste backloggd

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 console.log("Site carregado.");
+fetch("https://backloggd.com/u/Rafakkj0/rss/")
+    .then(response => response.text())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error(error);
+    });
