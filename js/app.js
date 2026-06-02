8385b0a7fa5125f0018dce286579899b0f5decaa
Rafakkj0 <134242702+Rafakkj0@users.noreply.github.com>
2026-06-02 11:24:37 -0300

    .

--- js/app.js
+++
@@ -1,8 +0,0 @@
-fetch("https://backloggd.com/u/Rafakkj0/reviews/rss/")
-    .then(response => response.text())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.error(error);
-    });
